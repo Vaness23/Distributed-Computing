c4c8588a7a15b859dec4a62f9c5b176a239fbe61
Vaness23 <ivan_b_23@mail.ru>
2021-02-25 19:18:22 +0300

    Client: Checking validity of the input number

--- achievement-2/client/client.py
+++ achievement-2/client/client.py
@@ -1,9 +1,19 @@
 import requests
 
-url = "http://localhost"
-myjson = input()
+url = "http://localhost"  # nginx url
 
-req = requests.post(url, json=myjson)
+while True:
+    number = input()
 
-print(req.status_code)
-print(req.json())
+    # checking if number consists of digits and doesn't begin with 0 (if it's not 0 itself)
+    if number.isdigit() and (len(number) == 1 or len(number) > 1 and number[0] != '0'):
+        
+        # forming json string and posting it
+        post_json = '{"number":"' + str(number) + '"}'
+        req = requests.post(url, json=post_json)
+
+        # printing obtained data
+        print(req.status_code)
+        print(req.json())
+    else:
+        print("Error: Input a valid number")
